ab0108b258e8d3757752ba8ebb4230a1bc45445e
Kimpix <kimpix@hotmail.fr>
2026-04-15 13:20:29 +0200

    Quartz sync: Apr 15, 2026, 1:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "kimpix.github.io/myDigitalGarden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
